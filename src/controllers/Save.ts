3ad4949b4cbf7bd1b78dfb151a546f1030741d2b
Alaa Barka <tmakaveli643@gmail.com>
2023-06-08 08:34:20 +0100

    save controller

--- src/controllers/Save.ts
+++ src/controllers/Save.ts
@@ -1,3 +1,20 @@
 import * as Express from 'express'
 import * as Services from './../services/countries.services'
 
+
+const Create : Express.RequestHandler = async(req, res) =>{
+    
+    const datas = req.body
+    if (!datas) {
+        res.status(400).send("Bad request")
+        return 
+    }
+    await Services.Save(datas).then(resp=>{
+        res.status(200).send("Done saving the country")
+    }).catch(e=>{
+        res.status(500).send("Error saving the country")
+        console.log(e)
+    })
+}
+
+export default Create
