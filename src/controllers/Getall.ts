19173166d8ea64625cc00d215af8684000641e58
Alaa Barka <tmakaveli643@gmail.com>
2023-06-08 08:29:16 +0100

    get all controller

---
+++ src/controllers/Getall.ts
@@ -0,0 +1,18 @@
+import * as Express from 'express'
+import * as Services from './../services/countries.services'
+
+
+const GetAll:Express.RequestHandler = async (req, res) =>{
+
+
+    const datas : any = Services.GetAll()
+    if (datas.length ===0) {
+        res.status(404).send("The database is empty")
+        return 
+
+    }
+    res.status(200).send(datas)
+}
+
+
+export default GetAll 
