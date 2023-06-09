c0da1e8d891785c2d886efa0c2d4d4e4f3f8b5d4
Alaa Barka <tmakaveli643@gmail.com>
2023-06-09 17:18:37 +0100

    fixing the filtering

--- src/controllers/Filter.ts
+++ src/controllers/Filter.ts
@@ -4,12 +4,15 @@ import * as Services from './../services/countries.services'
 
 const Filter:Express.RequestHandler = async (req, res)=>{
 
-    const field = req.query.field
-    const target = req.query.target
+    let field : any = req.query.field
+    field = field.toLowerCase()
+    let target :any = req.query.target
     if (!field || !target) {
         res.status(400).send("Bad request")
         return 
     }
+    target = ["iso1","iso2","currency"].includes(field) ? target.toUpperCase() : target[0].toUpperCase() + target.slice(1).toLowerCase() 
+    console.log(field, " ", target)
     const datas : any = await Services.Filter(field,target)
     if (datas.length ===0) {
         res.status(404).send("No results")
